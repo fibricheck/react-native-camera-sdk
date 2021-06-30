2d50657c0955e93fdf06de14737fa76d806f72fa
jan van der taelen <jan@pointbreak.co>
2021-06-30 16:16:38 +0200

    PackageList + compile files

--- android/src/main/java/com/fibricheckreactnativesdk/fibribridge/FibriBridgePackage.java
+++ android/src/main/java/com/fibricheckreactnativesdk/fibribridge/FibriBridgePackage.java
@@ -2,11 +2,16 @@
 
 package com.fibricheckreactnativesdk.fibribridge;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FibriBridgePackage implements ReactPackage {
@@ -14,16 +19,18 @@ public class FibriBridgePackage implements ReactPackage {
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     // Register your native module
     // https://facebook.github.io/react-native/docs/native-modules-android.html#register-the-module
-    return Arrays.<NativeModule>asList(
-        new FibriBridgeModule(reactContext)
-    );
+    List<NativeModule> modules = new ArrayList<>();
+
+    modules.add(new FibriBridgeModule(reactContext));
+
+    return modules;
+
   }
 
+  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    // Register your native component's view manager
-    // https://facebook.github.io/react-native/docs/native-components-android.html#4-register-the-viewmanager
-    return Arrays.<ViewManager>asList(
-        new FibriBridgeManager()
-    );
+       return Arrays.asList(
+           new FibriBridgeManager()
+       );
   }
 }
