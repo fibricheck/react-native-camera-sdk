ea9e02015f169fd43d531091d55aeaeb666438db
Jens Verbeken <jens.verbeken@gmail.com>
2021-10-12 17:00:45 +0200

    fix ts

--- src/FibriCheckView.tsx
+++ src/FibriCheckView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import { requireNativeComponent, ViewPropTypes } from 'react-native';
 
 interface FibriCheckViewProps {
