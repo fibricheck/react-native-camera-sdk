6862662e698dbc9059572950f8aae486803af2b0
Jens Verbeken <jens.verbeken@gmail.com>
2021-10-12 17:40:25 +0200

    remove prop-types

--- src/FibriCheckView.tsx
+++ src/FibriCheckView.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import { requireNativeComponent, ViewPropTypes } from 'react-native';
+import { requireNativeComponent } from 'react-native';
 
 interface FibriCheckViewProps {
   style: any;
-  graphBackgroundColor: string;
+  graphBackgroundColor?: string;
   drawGraph?: boolean;
   lineColor?: string;
   lineThickness?: number;
@@ -57,37 +56,7 @@ FibriCheckView.defaultProps = {
   waitForStartRecordingSignal: false,
 };
 
-FibriCheckView.propTypes = {
-  ...ViewPropTypes,
-  style: PropTypes.any,
-  drawGraph: PropTypes.bool,
-  lineColor: PropTypes.string,
-  lineThickness: PropTypes.number,
-  graphBackgroundColor: PropTypes.string,
-  drawBackground: PropTypes.bool,
-  sampleTime: PropTypes.number,
-  flashEnabled: PropTypes.bool,
-  gravEnabled: PropTypes.bool,
-  gyroEnabled: PropTypes.bool,
-  accEnabled: PropTypes.bool,
-  rotationEnabled: PropTypes.bool,
-  movementDetectionEnabled: PropTypes.bool,
-  fingerDetectionExpiryTime: PropTypes.number,
-  waitForStartRecordingSignal: PropTypes.bool,
-  onSampleReady: PropTypes.func,
-  onFingerDetected: PropTypes.func,
-  onFingerRemoved: PropTypes.func,
-  onCalibrationReady: PropTypes.func,
-  onHeartBeat: PropTypes.func,
-  onTimeRemaining: PropTypes.func,
-  onMeasurementFinished: PropTypes.func,
-  onMeasurementStart: PropTypes.func,
-  onFingerDetectionTimeExpired: PropTypes.func,
-  onPulseDetected: PropTypes.func,
-  onPulseDetectionTimeExpired: PropTypes.func,
-  onMovementDetected: PropTypes.func,
-  onMeasurementProcessed: PropTypes.func,
-};
+FibriCheckView.propTypes = {};
 
 const FibriCheck = requireNativeComponent('FibriCheck', FibriCheckView);
 
