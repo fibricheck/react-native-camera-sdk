86bd2709781ff70a6130126364ebeb474a890dde
Jens Verbeken <jens.verbeken@gmail.com>
2021-11-02 15:36:18 +0100

    🐛 add empty array

--- src/FibriCheckView.tsx
+++ src/FibriCheckView.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import {requireNativeComponent, UIManager, findNodeHandle} from 'react-native'
+import {
+  requireNativeComponent,
+  UIManager,
+  findNodeHandle,
+} from 'react-native';
 
 interface FibriCheckViewProps {
   style: any;
@@ -37,18 +41,17 @@ interface FibriCheckViewProps {
 }
 
 const FibriCheckView = (props: FibriCheckViewProps) => {
-
   React.useEffect(() => {
     return function cleanup() {
       // @ts-ignore
       UIManager.dispatchViewManagerCommand(
-          findNodeHandle(this),
-          // @ts-ignore
-          UIManager.FibriCheck.Commands.resetModule,
-          [],
+        findNodeHandle(this),
+        // @ts-ignore
+        UIManager.FibriCheck.Commands.resetModule,
+        []
       );
     };
-  });
+  }, []);
 
   return <FibriCheck {...props} />;
 };
