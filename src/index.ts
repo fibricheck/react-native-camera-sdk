e6fd33ffca4a47cf96db70cfba3fbc07ca73b3b5
Jens Verbeken <jens.verbeken@gmail.com>
2021-11-08 17:59:15 +0100

    add CameraData interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
 import RNFibriCheckView from './FibriCheckView';
-export { RNFibriCheckView };
+
+interface MotionData {
+  x: number[];
+  y: number[];
+  z: number[];
+} 
+
+interface Yuv { 
+  u: number[];
+  v: number[];
+  y: number[];
+}
+
+interface CameraData {
+  acc?: MotionData;
+  rotation?: MotionData;
+  grav?: MotionData;
+  gyro?: MotionData;
+  heartrate: number;
+  measurementTimestamp: Date;
+  quadrants: Yuv[][];
+  technicalDetails: {
+    camera_exposure_time: number;
+    camera_hardware_level: string;
+    camera_iso: number;
+  }
+  time: number[];
+  yList: number[]
+}
+
+export { RNFibriCheckView, CameraData };
