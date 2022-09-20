9ad24548a9fc503cc3265e9a05cab758921a4bfa
Thomas Reyskens <thomas.reyskens@fibricheck.com>
2022-09-20 09:56:39 +0200

    FCS-19 iOS Header fixes

--- ios/RCTFibriCheck/FibriCheckerComponent/Measuring/MeasurementController.h
+++ ios/RCTFibriCheck/FibriCheckerComponent/Measuring/MeasurementController.h
@@ -80,12 +80,12 @@ typedef NS_ENUM(NSInteger, MeasurementControllerEvent) {
 - (void)measurementController:(MeasurementController *)measurementController progressUpdated:(NSUInteger)elapsedTime;
 - (void)measurementController:(MeasurementController *)measurementController heartRateUpdated:(NSUInteger)heartRate;
 - (void)measurementController:(MeasurementController *)measurementController didReceiveMeasurementError:(NSString*)message;
+- (void)measurementController:(MeasurementController *)measurementController didReceiveFingerRemoved:(DataPoint *)datapoint;
 
 - (void)measurementControllerDidStartRecording;
 - (void)measurementControllerDidReceiveError;
 - (void)measurementControllerDidReceiveMovement;
 - (void)measurementControllerDidReceiveFingerDetected;
-- (void)measurementControllerDidReceiveFingerRemoved;
 - (void)measurementControllerDidReceivePulseDetected;
 - (void)measurementControllerDidReceivePulseDetectionTimeout;
 - (void)measurementControllerDidReceiveFingerDetectionTimeout;
